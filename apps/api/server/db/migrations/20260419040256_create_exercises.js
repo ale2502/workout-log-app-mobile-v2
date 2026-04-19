09784fa778217d56951d0cc84e9bffb0e19ba60f
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-04-19 20:01:20 +1200

    create exercises table

---
+++ apps/api/server/db/migrations/20260419040256_create_exercises.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function (knex) {
+  return knex.schema.createTable('exercises', (table) => {
+    table.increments('id');
+    table.string('name');
+    table.string('muscle_group');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function (knex) {
+  return knex.schema.dropTable('exercises');
+};

b3ccb938bf2fe1b9e28feb4c152d80c2171cb1f0
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-04-29 22:37:53 +1200

    Work on muscle-groups.tsx

--- apps/mobile/app/workout/muscle-groups.tsx
+++ apps/mobile/app/workout/muscle-groups.tsx
@@ -2,4 +2,28 @@ import { useEffect, useState } from 'react';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-export default function MuscleGroupScreen() {}
+type Exercise = {
+  id: number;
+  name: string;
+  muscleGroup: string;
+};
+
+export default function MuscleGroupScreen() {
+  const params = useLocalSearchParams<{ workoutId: string }>();
+  const workoutId = params.workoutId;
+  // Same as:
+  // const { workoutId } = useLocalSearchParams<{ workoutId: string }>();
+  const [exercises, setExercises] = useState<Exercise[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function loadExercises() {
+      const response = await fetch('http://192.168.1.205:3001/exercises');
+
+      if (!response.ok) {
+        throw new Error('Could not load exercises');
+      }
+    }
+  });
+}

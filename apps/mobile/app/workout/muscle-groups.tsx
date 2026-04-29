f2a3e6513074a28bb68d9a76412f0983b0b5bde4
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-04-29 21:44:21 +1200

    Create muscle-group.tsx page

---
+++ apps/mobile/app/workout/muscle-groups.tsx
@@ -0,0 +1,5 @@
+import { useEffect, useState } from 'react';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+export default function MuscleGroupScreen() {}

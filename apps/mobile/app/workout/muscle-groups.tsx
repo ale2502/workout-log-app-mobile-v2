6a61034768ca9bee8c604d60b0c6afff289da61b
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-05-01 11:54:44 +1200

    Progress on muscle_groups.tsx

--- apps/mobile/app/workout/muscle-groups.tsx
+++ apps/mobile/app/workout/muscle-groups.tsx
@@ -19,11 +19,26 @@ export default function MuscleGroupScreen() {
 
   useEffect(() => {
     async function loadExercises() {
-      const response = await fetch('http://192.168.1.205:3001/exercises');
+      try {
+        const response = await fetch('http://192.168.1.205:3001/exercises');
 
-      if (!response.ok) {
-        throw new Error('Could not load exercises');
+        if (!response.ok) {
+          throw new Error('Failed to load muscle-groups');
+        }
+
+        const data = await response.json();
+        setExercises(data);
+      } catch (error) {
+        setError('Could not load muscle-groups');
+      } finally {
+        setIsLoading(false);
       }
     }
-  });
+    loadExercises();
+    // [] is a dependency array, []: run once, [someValue]: run once then again if someValue changes, no array: run after every render
+  }, []);
+
+  const muscleGroups = [
+    ...new Set(exercises.map((exercise) => exercise.muscleGroup)),
+  ];
 }

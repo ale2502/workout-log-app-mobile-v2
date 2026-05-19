37eac38e18837c6176c9116f691f96753ecc4009
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-05-19 17:48:01 +1200

    Create component main function

--- apps/mobile/components/workout/NumberStepperInput.tsx
+++ apps/mobile/components/workout/NumberStepperInput.tsx
@@ -8,3 +8,27 @@ type NumberStepperInputProps = {
   onIncrease: () => void;
   placeholder: string;
 };
+
+export function NumberStepperInput(props: NumberStepperInputProps) {
+  return (
+    <View style={styles.inputRow}>
+      <Text style={styles.label}>{props.label}</Text>
+
+      <TextInput
+        style={[styles.formInput, styles.rowInput]}
+        value={props.value}
+        onChangeText={props.onChangeText}
+        placeholder={props.placeholder}
+        keyboardType="numeric"
+      />
+
+      <Pressable style={styles.incrementButton} onPress={props.onDecrease}>
+        <Text style={styles.incrementText}>-</Text>
+      </Pressable>
+
+      <Pressable style={styles.incrementButton} onPress={props.onIncrease}>
+        <Text style={styles.incrementText}>+</Text>
+      </Pressable>
+    </View>
+  );
+}

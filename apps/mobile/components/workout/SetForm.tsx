2a24ccdc80a80626f099a27987dfdb8b3816eec6
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-05-23 22:30:44 +1200

    Create SetForm component and its props type

---
+++ apps/mobile/components/workout/SetForm.tsx
@@ -0,0 +1,24 @@
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { NumberStepperInput } from './NumberStepperInput';
+
+type SetFormProps = {
+  reps: string;
+  load: string;
+  rir: string;
+  note: string;
+
+  onChangeReps: (newValue: string) => void;
+  onChangeLoad: (newValue: string) => void;
+  onChangeRir: (newValue: string) => void;
+  onChangeNote: (newValue: string) => void;
+
+  onDecreaseReps: () => void;
+  onIncreaseReps: () => void;
+  onDecreaseLoad: () => void;
+  onIncreaseLoad: () => void;
+  onDecreaseRir: () => void;
+  onIncreaseRir: () => void;
+
+  onSave: () => void;
+  isSaving: boolean;
+};

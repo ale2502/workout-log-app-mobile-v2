86e2006a84ed2cea0a93634152465d9595c69dd3
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-05-31 16:26:22 +1200

    Add comment

--- apps/api/server/models/workout.ts
+++ apps/api/server/models/workout.ts
@@ -2,6 +2,7 @@ export interface WorkoutData {
   performedOn: string;
 }
 
+// createdAt exists only after the DB automatically creates it
 export interface Workout extends WorkoutData {
   id: number;
   createdAt: string;

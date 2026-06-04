988b57e2c1edc8ff79e2056327c4546b4403da9b
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-06-05 10:31:26 +1200

    Create [workoutId] page file and imports

---
+++ apps/mobile/app/workout/[workoutId].tsx
@@ -0,0 +1,4 @@
+import { useEffect, useState } from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+import { SetDisplay } from '../../../api/server/models/set';

2dd6bb08120f04c3adc13c5c095c9a5a5a2e470e
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-06-04 15:08:27 +1200

    Add get route for getSetsByWorkoutId to workouts.ts route file

--- apps/api/server/routes/workouts.ts
+++ apps/api/server/routes/workouts.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as db from '../db/workouts';
+import * as setDb from '../db/sets';
 
 const router = Router();
 
@@ -21,6 +22,24 @@ router.get('/', async (req, res) => {
     const workoutsArr = await db.getWorkouts();
     console.log(workoutsArr);
     res.status(200).json(workoutsArr);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+router.get('/:id/sets', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+
+    if (Number.isNaN(id)) {
+      res.status(400).json({ error: 'set id must be a number' });
+      return;
+    }
+
+    const workoutSets = await setDb.getSetsByWorkoutId(id);
+
+    res.status(200).json(workoutSets);
   } catch (error) {
     console.error(error);
     res.status(500).send('Something went wrong');

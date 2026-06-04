b20224be33fe4a9052309b52f5ed1bd498ede262
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-06-05 11:06:22 +1200

    Set params and states

--- apps/mobile/app/workout/[workoutId].tsx
+++ apps/mobile/app/workout/[workoutId].tsx
@@ -2,3 +2,12 @@ import { useEffect, useState } from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 import { SetDisplay } from '../../../api/server/models/set';
+
+export default function WorkoutDetailScreen() {
+  const params = useLocalSearchParams<{ workoutId: string }>();
+  const workoutId = params.workoutId;
+
+  const [sets, setSets] = useState<SetDisplay[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+}

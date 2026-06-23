70be11d84e71b11667fb5b854d58036588f17b28
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-06-24 08:24:19 +1200

    Create add new exercise route

--- apps/api/server/routes/exercises.ts
+++ apps/api/server/routes/exercises.ts
@@ -13,14 +13,28 @@ router.get('/', async (req, res) => {
   }
 });
 
+// POST - Create new exercise
 router.post('/', async (req, res) => {
   try {
     const newExercise = req.body;
 
     if (
-      newExercise.name === undefined || newExercise.muscleGroup === undefined
-    )
+      newExercise.name === undefined ||
+      newExercise.muscleGroup === undefined
+    ) {
+      res.status(400).json({
+        error: 'name and muscleGroup are required',
+      });
+      return;
+    }
+
+    const exercise = await db.addNewExercise(newExercise);
+    console.log(exercise);
+    res.status(201).json(exercise);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Something went wrong');
   }
-})
+});
 
 export default router;

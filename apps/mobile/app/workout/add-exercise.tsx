8981b6e10921c580ce9faa4d02bd2c730ff36d10
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-06-24 11:09:57 +1200

    Create add-exercise page

---
+++ apps/mobile/app/workout/add-exercise.tsx
@@ -0,0 +1,5 @@
+import { useState } from 'react';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+export default function AddExerciseScreen() {}

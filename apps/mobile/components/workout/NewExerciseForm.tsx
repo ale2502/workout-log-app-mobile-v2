1e0cb710aab4a19aa5eb05242043d16c80fcc95a
Alessandro Jablonski <ale_jablonski@hotmail.com>
2026-06-25 14:38:27 +1200

    Create NewExerciseForm type props

--- apps/mobile/components/workout/NewExerciseForm.tsx
+++ apps/mobile/components/workout/NewExerciseForm.tsx
@@ -3,4 +3,7 @@ import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 type NewExerciseFormProps = {
   exerciseName: string;
   muscleGroup: string;
+  onChangeExerciseName: (newValue: string) => void;
+  onSubmit: () => void;
+  isSaving: boolean;
 };
